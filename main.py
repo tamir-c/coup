0353520762f1c5b506b3b7cdb05e39f1efc1fa94
Tamir Cohen <tamirc1234@gmail.com>
2023-01-30 23:37:12 +0000

    Generates all possible actions and counteractions for given game state

--- main.py
+++ main.py
@@ -46,60 +46,173 @@ def main():
         def __repr__(self):
             return self.name
 
+    def check_player_in(player):
+        if len(player.cards) <= 0:
+            return False
+        return True
+
+    def check_coup(player, target):
+        if len(player.cards) <= 0:
+            return False
+        if len(target.cards) <= 0:
+            return False
+        if player.coins < 7:
+            return False
+        return True
+
+    def check_assassinate(player, target):
+        if len(player.cards) <= 0:
+            return False
+        if len(target.cards) <= 0:
+            return False
+        if player.coins < 3:
+            return False
+        return True
+
+    def check_steal(player, target):
+        if len(player.cards) <= 0:
+            return False
+        if len(target.cards) <= 0:
+            return False
+        if player.coins < 1:
+            return False
+        return True
+    
+    def get_actions(current_player_index, players):
+        actions = []
+        player = players[current_player_index]
+        if not check_player_in(player):
+            return actions
+        force_coup = False
+        if player.coins >= 10:
+            force_coup = True
+        if not force_coup:
+            actions.append(Action("Income", player))
+            actions.append(Action("Foreign Aid", player))
+            actions.append(Action("Tax", player))
+            actions.append(Action("Exchange", player))
+            for i in range(len(players)):
+                if i == current_player_index:
+                    continue
+                if check_player_in(players[i]): # double checking player is in, this is already checked in individual action check functions
+                    if check_coup(player, players[i]):
+                        actions.append(Action("Coup", player, players[i]))
+                    if check_assassinate(player, players[i]):
+                        actions.append(Action("Assassinate", player, players[i]))
+                    if check_steal(player, players[i]):
+                        actions.append(Action("Steal", player, players[i]))
+        else:
+            for i in range(len(players)):
+                if i == current_player_index:
+                    continue
+                if check_player_in(players[i]): # double checking player is in, this is already checked in individual action check functions
+                    if check_coup(player, players[i]):
+                        actions.append(Action("Coup", player, players[i]))
+
+        return actions
+
+    def income(player):
+        player.coins += 1
+    def foreign_aid(player):
+        player.coins += 2
+    def coup(player, target):
+        player.coins -= 7
+        target.cards.pop(0) # change this to allow target to choose which card to give up
+    def tax(player):
+        player.coins += 3
+    def assassinate(player, target):
+        player.coins -= 3
+        target.cards.pop(0) # change this to allow target to choose which card to give up
+    def steal(player, target):
+        coins_to_take = 2
+        if target.coins == 1:
+            coins_to_take = 1
+        target.coins -= coins_to_take
+        player.coins += coins_to_take
+    def exchange(player, deck):
+        deck.deal(player, 2)
+        player.cards.pop(0) # change this to allow player to choose which cards to keep
+        player.cards.pop(0)
+
+    def get_counteractions(actor_player_index, players, action):
+        counteractions = []
+        player = players[actor_player_index]
+        if not check_player_in(player): # This should never happen
+            return False
+        for i in range(len(players)):
+            if i == actor_player_index:
+                continue
+            if action.name == "Foreign Aid":
+                counteractions.append(Counteraction(player, players[i], action, "Duke"))
+        if action.name == "Assassinate":
+            counteractions.append(Counteraction(player, action.target, action, "Contessa"))
+        elif action.name == "Steal":
+            counteractions.append(Counteraction(player, action.target, action, "Ambassador"))
+            counteractions.append(Counteraction(player, action.target, action, "Captain"))
+
+        return counteractions
+
+    class Counteraction(object):
+        def __init__(self, actor, counteractor, action, claim):
+            self.actor = actor
+            self.counteractor = counteractor
+            self.action = action
+            self.claim = claim
+        def __repr__(self):
+            return self.counteractor.name + " blocks " + self.action.__repr__() + " claiming " + self.claim
+
     class Action(object):
-        def __init__(self, type):
-            self.type = type
-            action_name = ""
+        def __init__(self, action_name, player, target=None):
+
             action_character = "None"
-            blocked_by = ""
-            self.blocked_by
-            cost = 0
-            if self.type == 0:
-                action_name = "Income"
-            elif self.type == 1:
-                action_name = "Foreign Aid"
-            elif self.type == 2:
-                action_name = "Coup"
-            elif self.type == 3:
-                action_name = "Tax"
+            blocked_by = []
+
+            if action_name == "Foreign Aid":
+                blocked_by.append("Duke")
+
+            elif action_name == "Tax":
                 action_character = "Duke"
-            elif self.type == 4:
-                action_name = "Assassinate"
+
+            elif action_name == "Assassinate":
                 action_character = "Assassin"
-            elif self.type == 5:
-                action_name = "Steal"
+                blocked_by.append("Contessa")
+
+            elif action_name == "Steal":
                 action_character = "Captain"
-            elif self.type == 6:
-                action_name = "Exchange"
+                blocked_by.append("Ambassador")
+                blocked_by.append("Captain")
+
+            elif action_name == "Exchange":
                 action_character = "Ambassador"
             
             self.name = action_name
-            self.action_character = "action_character"
+            self.player = player
+            self.target = target
+            self.action_character = action_character
             self.blocked_by = blocked_by
 
-        def income(player):
-            player.coins += 1
-        def foreign_aid(player):
-            player.coins += 2
-        def coup(player, target):
-            if player.coins < 7:
-                return False
-            player.coins -= 7
-            target.cards.pop(0) # change this to allow target to choose which card to give up
+        def __repr__(self):
+            if self.target:
+                if self.name == "Steal":
+                    return self.name + " from " + self.target.name
+                return self.name + " " + self.target.name
+            return self.name
 
     deck = Deck()
     deck.shuffle()
     players = []
-    num_players = 2
+    num_players = 3
     for player_num in range(num_players):
         name = "Player " + str(player_num)
         players.append(Player(name))
         deck.deal(players[player_num], times=2)
 
     print(players)
-    print(players[0].cards)
-    print(players[1].cards)
-    print(deck)
+    a = get_actions(0, players)
+    print(a)
+    a1 = a[1]
+    print(a1)
+    print(get_counteractions(0, players, a1))
 
 if __name__ == "__main__":
     main()
