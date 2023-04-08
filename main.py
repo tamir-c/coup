9120834301d00ddd1429754cb09fcbed55664a74
Tamir Cohen <sc20tc@leeds.ac.uk>
2023-04-08 02:25:26 +0100

    Made order of responses to actions random for random agents

--- main.py
+++ main.py
@@ -25,14 +25,23 @@ def game(state):
             print("Possible actions:")
             print(actions)
             print("Action taken: " + state.players[turn].name + " plays '" + action.__repr__() + "' claiming " + action.action_character)
-
-            for p in state.players:
-                action_challenges = get_action_challenges(action, p, state.players)
-                print("Possible challenges for " + p.name + ":")
+            r = list(range(state.num_players))
+            random.shuffle(r)
+            for i in r:
+                action_challenges = get_action_challenges(action, state.players[i], state.players)
+                print("Possible challenges for " + state.players[i].name + ":")
                 print(action_challenges)
-                action_challenge = p.agent.choice(action_challenges, "Please choose action challenge from:")
+                action_challenge = state.players[i].agent.choice(action_challenges, "Please choose action challenge from:")
                 if not (action_challenge == None): # if a player has challenged, break - only one player can challenge an action in a play
                     break
+                
+            # for p in state.players:
+            #     action_challenges = get_action_challenges(action, p, state.players)
+            #     print("Possible challenges for " + p.name + ":")
+            #     print(action_challenges)
+            #     action_challenge = p.agent.choice(action_challenges, "Please choose action challenge from:")
+            #     if not (action_challenge == None): # if a player has challenged, break - only one player can challenge an action in a play
+            #         break
 
             print("Challenge taken:")
             print(action_challenge)
@@ -54,25 +63,43 @@ def game(state):
                     print(winner.name + " wins the challenge because " + loser.name + " does not have " + action.action_character)
                     loser.lose_influence()
             else:
-                for p in state.players:
-                    counteractions = get_counteractions(p, state.players, action)
-                    print("Possible counteractions for " + p.name + ":")
+                random.shuffle(r)
+                for i in r:
+                    counteractions = get_counteractions(state.players[i], state.players, action)
+                    print("Possible counteractions for " + state.players[i].name + ":")
                     print(counteractions)
-                    counteraction = p.agent.choice(counteractions, "Please choose counteraction from:")
+                    counteraction = state.players[i].agent.choice(counteractions, "Please choose counteraction from:")
                     print("Counteraction taken: ")
                     print(counteraction)
                     if not (counteraction == None):
                         break
+                # for p in state.players:
+                #     counteractions = get_counteractions(p, state.players, action)
+                #     print("Possible counteractions for " + p.name + ":")
+                #     print(counteractions)
+                #     counteraction = p.agent.choice(counteractions, "Please choose counteraction from:")
+                #     print("Counteraction taken: ")
+                #     print(counteraction)
+                #     if not (counteraction == None):
+                #         break
                 if counteraction == None: # the action went unchallenged so if no one counteracts the action succeeds 
                     action.execute(success=True)
                 else:
-                    for p in state.players:
-                        counteraction_challenges = get_counteraction_challenges(counteraction, p, state.players)
-                        print("Possible challenges to counteraction for " + p.name + ":")
+                    random.shuffle(r)
+                    for i in r:
+                        counteraction_challenges = get_counteraction_challenges(counteraction, state.players[i], state.players)
+                        print("Possible challenges to counteraction for " + state.players[i].name + ":")
                         print(counteraction_challenges)
-                        counteraction_challenge = p.agent.choice(counteraction_challenges, "Please choose counteraction challenge from:")
+                        counteraction_challenge = state.players[i].agent.choice(counteraction_challenges, "Please choose counteraction challenge from:")
                         if not (counteraction_challenge == None):
                             break
+                    # for p in state.players:
+                    #     counteraction_challenges = get_counteraction_challenges(counteraction, p, state.players)
+                    #     print("Possible challenges to counteraction for " + p.name + ":")
+                    #     print(counteraction_challenges)
+                    #     counteraction_challenge = p.agent.choice(counteraction_challenges, "Please choose counteraction challenge from:")
+                    #     if not (counteraction_challenge == None):
+                    #         break
                     print("Challenge to counteraction taken:")
                     print(counteraction_challenge)
                     if not (counteraction_challenge == None):
@@ -97,14 +124,15 @@ def game(state):
                         action.execute(success=False)
         
         state.increment_turn()
+
     winner = state.players[get_winner(state.players)]
     print("\n" + winner.name + " wins the game!!!")
     enablePrint()
     return winner
 
 def main():
-    iterations = 500
-    num_players=6
+    iterations = 10000
+    num_players=2
     results = [0 for i in range(num_players)]
     for i in range(iterations):
         s = State(num_players=num_players)
